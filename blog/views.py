e28fec2f87d4be79c9915478f858eb18ae7e6a78
Julio Sarango <jsarangoq@gmail.com>
2024-06-10 15:46:01 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -6,20 +6,18 @@ import logging
 from blog.models import Post
 from blog.forms import CommentForm
 
+logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+
 def index(request):
-  logger = logging.getLogger(__name__)
-
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts))
 
   return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    logger = logging.getLogger(__name__)
+    post = get_object_or_404(Post, slug=slug)    
     if request.user.is_active:
         if request.method == "POST":
             comment_form = CommentForm(request.POST)
