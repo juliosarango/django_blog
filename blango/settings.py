b3f680bdacfe4a0a872f0f8e67849b792e88066e
Julio Sarango <jsarangoq@gmail.com>
2024-06-14 21:32:29 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -43,10 +43,11 @@ class Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
+      'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
-      'debug_toolbar'
+      'debug_toolbar',      
   ]
 
   MIDDLEWARE = [
@@ -192,6 +193,8 @@ class Dev(Configuration):
   }
 
   DJANGO_ADMINS="Ben Shaw,ben@example.com;Leo Lucio,leo@example.com"
+
+  AUTH_USER_MODEL = "blango_auth.User"
 
 class Prod(Dev):
   DEBUG = False
