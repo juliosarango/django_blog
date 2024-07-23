697eef0cacf872bbedb135a7d4dac8ab4189b452
Julio Sarango <jsarangoq@gmail.com>
2024-07-23 21:35:51 +0000

    Finish nested relationsips

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 
-from blog.api.serializers import PostSerializer, UserSerializer
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
 from blog.models import Post
 from blango_auth.models import User
 
@@ -11,9 +11,11 @@ class PostList(generics.ListCreateAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
-  serializer_class = PostSerializer
+  serializer_class = PostDetailSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
