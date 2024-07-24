d2c63837a2fcd923f1be36376320e5c0c76c8701
Julio Sarango <jsarangoq@gmail.com>
2024-07-24 16:19:24 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -55,6 +55,7 @@ class Dev(Configuration):
       'allauth.socialaccount.providers.google',
       'rest_framework',
       'rest_framework.authtoken',
+      'drf_yasg'
   ]
 
   MIDDLEWARE = [
@@ -219,6 +220,13 @@ class Dev(Configuration):
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ]
+  }
+
+  SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+        "Basic": {"type": "basic"},
+    }
   }
 
 
